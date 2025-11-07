56b04518b0c82eeb63f1d80da7ff9c6437222bac
verdent-slash <ajma2023-5333-37619@bicol-u.edu.ph>
2025-11-07 18:45:16 +0800

    initial commit

--- app/src/main/java/com/example/albaymap/MainActivity.kt
+++ app/src/main/java/com/example/albaymap/MainActivity.kt
@@ -4,7 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
